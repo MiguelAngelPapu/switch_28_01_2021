7b38d2c09d51876e2b155e2964a8b206f3f0b63b
Miguel Angel Castro Escamilla <ma98030965140@gmail.com>
2021-01-28 19:00:57 -0500

    switch_28_01_2021

---
+++ main.js
@@ -0,0 +1,22 @@
+let key = prompt("Ingrese una opcion");
+switch (key) {
+    case "1":
+        console.log(`Opcion uno`);
+        break;
+    case "2":
+        console.log(`Opcion dos`);
+        break;
+    default:
+        console.log(`Opcion incorrecta`);
+        break;
+}
+
+
+
+
+
+
+
+
+
+
